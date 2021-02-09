2a4fe46ee20dd86fa42f042d5448f8f3132f90a5
HolyPi <joanelly2018@gmail.com>
2021-02-09 15:43:09 -0500

    Added index.js + tests

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const { months, mons, days, dys } = require('./utils')
+
+class D {
+    constructor(...args) {
+        this._date = new Date(...args)
+    }
+
+    get year() {
+        return this._date.getFullYear()
+    }
+    get yr() {
+        return this._date.getFullYear() % 100
+    }
+
+    get month() {
+        return months[this._date.getMonth()]
+    }
+
+    get mon() {
+        return mons[this._date.getMonth()]
+    }
+
+    get day() {
+        return days[this._date.getDay()]
+    }
+
+    get dy() {
+        return dys[this._date.getDay()]
+    }
+    get date() {
+        return this._date.getDate()
+    }
+}
+
+module.exports = D
+
+// const d = new D()
+// console.log(d.year)
+// console.log(d.yr)
---
+++ tests/index.test.js
@@ -0,0 +1,36 @@
+const D = require('../src/index')
+const { months, mons, days, dys } = require('../src/utils')
+const today = new Date()
+const d = new D()
+const bDay = new D(1965, 8, 26)
+
+test('D.year', () => {
+    expect( d.year ).toBe( today.getFullYear())
+    expect(bDay.year).toBe(1965)
+})
+
+test('D.year', () => {
+    expect( d.yr ).toBe( today.getFullYear() % 100)
+    expect(bDay.yr).toBe(65)
+})
+
+test('D.month', () => {
+    expect(bDay.month).toBe(months[8])
+})
+
+test('D.mon', () => {
+    expect(bDay.mon).toBe(mons[8])
+})
+
+
+test('D.day', () => {
+    expect(d.day).toBe(days[today.getDay()])
+})
+
+test('D.dy', () => {
+    expect(d.dy).toBe(dys[today.getDay()])
+})
+
+test('D.date', () => {
+    expect(d.date).toBe(today.getDate())
+})
